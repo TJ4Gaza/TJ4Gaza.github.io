1ddf5685834c68e8012f903096b9b7451f061367
Mostafa Hassan <transitionaljusticeforgaza@gmail.com>
2023-12-30 21:59:38 -0500

    Adjusting Footers & focusing the content

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,3 +5,30 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to open the popup
+    function openPopup() {
+        document.getElementById('popup').style.display = 'block';
+    }
+
+    // Function to close the popup
+    function closePopup() {
+        document.getElementById('popup').style.display = 'none';
+    }
+
+    // Event listener for the "here" link
+    document.getElementById('openPopup').addEventListener('click', openPopup);
+
+    // Event listener for the close button in the popup
+    document.querySelector('.popup-close').addEventListener('click', closePopup);
+
+    // Event listener for clicking outside the popup to close it
+    window.addEventListener('click', function (event) {
+        if (event.target == document.getElementById('popup')) {
+            closePopup();
+        }
+    });
+});
